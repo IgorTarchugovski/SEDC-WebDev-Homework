24025444e1878c6b28d8e36ab37adb6cea164bb9
Igor Tarchugovski <igor.t93@outlook.com>
2021-01-23 22:51:14 +0100

    largestValue_example

--- Basic JS Development - Java Script/Class04/Loops/Exercises/example.js
+++ Basic JS Development - Java Script/Class04/Loops/Exercises/example.js
@@ -1,1 +1,20 @@
 // Action: Write a JavaScript program to read ten array values, determine the largest value, and print it
+
+// let input = prompt("Enter an array of number's. Example: 23548651148654");
+// let myArr = input.split(',');
+let myArr = [1, 21, 48.222, 5, 84, 10, 7.5, 8, 999, 10];
+console.log(myArr);
+
+function largestValue(arr) {
+    let val = null;
+    for (i=0; i<arr.length; i++) {
+        if (arr[i] > val) {
+          val = arr[i]
+          
+        }
+        continue;
+    }
+    return val;
+}
+
+console.log(largestValue(myArr));
