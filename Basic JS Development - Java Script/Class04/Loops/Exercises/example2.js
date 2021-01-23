76f0bd6089bb328e122204b3e2ea9e81b5736809
Igor Tarchugovski <igor.t93@outlook.com>
2021-01-23 23:08:59 +0100

    numbers_digits

--- Basic JS Development - Java Script/Class04/Loops/Exercises/example2.js
+++ Basic JS Development - Java Script/Class04/Loops/Exercises/example2.js
@@ -1,3 +1,10 @@
 /*
 Action: Write a JavaScript program that will read in a number and write out its digits
 */
+
+let input = prompt("Enter a number to write out its digits:");
+let myArray = input.split('');
+
+for (let i=0; i<myArray.length; i++) {
+    console.log(`Digit no.${i+1}: ${myArray[i]}`);
+}
