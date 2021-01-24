44ec0e85e7e244dc6ae66e6485ba59db5e92a2d2
Igor Tarchugovski <igor.t93@outlook.com>
2021-01-24 11:56:38 +0100

    loops_studentExercise1-done

--- Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise1.js
+++ Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise1.js
@@ -8,19 +8,23 @@ Takes 2 arguments: number, array
 Calculates how many times the number is contained in an array with numbers
 Outputs the result to the screen like so: "There is 4 occurrences of number 5 in the array"
 Call the function three times with different arrays.
-Time to accomplish: 15 minutes.
-EXERCISE 1 SOLUTION
-function numberCount(number, array) {
-        let occurences = 0;
-        for(let i = 0; i < array.length; i++){
-            if(array[i] === number){
-                occurences++;
-            }
-        }
-        return occurences;
-    }
-    let input = parseInt(prompt("Enter a number:"));
-    let testArray = [2, 5, 6, 3, 5, 5, 2, 5, 7, 8, 2];
-    console.log(`There are ${numberCount(input, testArray)} occurences of ${input} in the array!`);
-
 */
+
+
+let action = prompt("STUDENT EXERCISE #1 \nEnter an array of numbers:");
+let arr = action.split('');
+console.log(arr)
+let num = parseInt(prompt("STUDENT EXERCISE #1 \nEnter a number:"));
+console.log(num);
+
+function findNumber(number, array) {
+    let occurences = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] == number){
+            occurences++;
+        }
+    }
+    return occurences;
+}
+
+console.log(`There are ${findNumber(num, arr)} occurences of ${num} in the array!`);
