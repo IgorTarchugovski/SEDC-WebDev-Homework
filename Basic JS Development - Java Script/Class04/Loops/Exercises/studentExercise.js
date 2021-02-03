d5513c5c7b961bb615aea0f095ea54db58999860
Igor Tarchugovski <igor.t93@outlook.com>
2021-02-03 21:02:09 +0100

    sumOfSquares

--- Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise.js
+++ Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise.js
@@ -4,3 +4,11 @@ Math.pow(x,y); - Returns the value of x to the power of y
 Action: Write a JavaScript program to write the sum of squares of the numbers from 101 to 150
 */
 
+let sumOfSquares = null;
+
+for (let i=101; i<=150; i++) {
+    sumOfSquares += Math.pow(i, 2);
+    
+}
+
+console.log(`The sum of squares of the numbers from 101 to 150 is: ${sumOfSquares}`);
