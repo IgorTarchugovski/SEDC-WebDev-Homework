73ef7207365632520c7b3e337f9f81c095b34b84
Igor <igor,93@outlook,com>
2021-03-12 12:16:43 +0100

    BJS_hw_loops

--- Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise2.js
+++ Basic JS Development - Java Script/Class04/Loops/Exercises/studentExercise2.js
@@ -8,19 +8,28 @@ If the type is even the function should find all the even numbers and return the
 If the type is odd the function should find all the odd numbers and return them as result
 Time to accomplish: 20 minutes.
 
-function filterOddEven(array, type){
-        let result = [];
-        if(type === "even"){
-           for (let num of array) {
-               if(num % 2 === 0){
-                   result.push(num);
-               }
-           } 
-           return result;
-        } else if(type === "odd"){
-            for (let num of array) {
-                if(num % 2 !== 0){
-                    result.push(num);
-                }
-            } 
 */
+
+let array = prompt("Enter an array of numbrs: ");
+let type = prompt("Type of numbers to be found: odd / even");
+
+function filterOddEven(array, type) {
+  let result = [];
+  if (type === "even") {
+    for (let num of array) {
+      if (num % 2 === 0) {
+        result.push(num);
+      }
+    }
+    return result;
+  } else if (type === "odd") {
+    for (let num of array) {
+      if (num % 2 !== 0) {
+        result.push(num);
+      }
+    }
+    return result;
+  }
+}
+
+console.log(filterOddEven(array, type));
